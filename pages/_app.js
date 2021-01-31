425cc654821ac519888eab66c887dcf6ed6b55cd
Marcelo Alves <mafeal@gmail.com>
2021-01-31 12:57:40 -0300

    Inclusão das tags og:image:width e og:image:height

--- pages/_app.js
+++ pages/_app.js
@@ -55,6 +55,14 @@ export default function App({ Component, pageProps }) {
           property="og:image"
           content="src\assets\imgs\img_site.JPG"
         />
+        <meta
+          property="og:image:width"
+          content="600px"
+        />
+        <meta
+          property="og:image:height"
+          content="600px"
+        />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
